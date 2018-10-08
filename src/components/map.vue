78923ed27df3d37e3ed8484e5a9c69f3b0d5465f
DeteMin <516346543@qq.com>
2018-10-08 17:00:07 +0800

    修改后的百度地图页面

---
+++ src/components/map.vue
@@ -0,0 +1,36 @@
+<template id="child-map">
+  <div id="map" class="mystyle"></div>
+</template>
+
+<script>
+import BMap from 'BMap'
+
+export default{
+  data () {
+    return {
+
+    }
+  },
+  props: ['m'],
+  mounted () {
+    this.mapInit()
+  },
+  methods: {
+    mapInit () {
+      const _this = this
+      const map = new BMap.Map('map')
+      _this.map = map
+      map.centerAndZoom(new BMap.Point(116.404, 39.915), 15)
+      map.enableScrollWheelZoom()
+    }
+  }
+}
+</script>
+<style>
+.mystyle {
+width: 100%;
+height: 600px;
+border: 1px solid #eee;
+}
+
+</style>
